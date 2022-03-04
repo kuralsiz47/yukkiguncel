dab3ab04c81c0e576f259d8a4904adfd9875f1fd
kuralsiz47 <98223394+kuralsiz47@users.noreply.github.com>
2022-03-05 00:20:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = "https://github.com/kuralsiz47/SiestaXMusic"
+UPSTREAM_REPO = "https://github.com/kuralsiz47/yukkiguncel"
 UPSTREAM_BRANCH = "main"
 
 SUPPORT_CHANNEL = "https://t.me/TechZBots"
